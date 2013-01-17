47a1a847eaf256e433a4e781273a101b3828eb7b
John Lyle <john.lyle@cs.ox.ac.uk>
2013-01-17 12:00:41 +0000

    Add config.xml, build/signing scripts and .gitignore
    Jira issue: APPPOL-2

---
+++ buildscripts/build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+THIS_SCRIPT=./buildscripts/build.sh
+
+UNSIGNED_APP=./build/policyeditor.wgt
+
+if [ ! -f $THIS_SCRIPT ]; then 
+    echo "This script has to be run from the app root directory"
+    exit -1;
+fi
+
+if [ -e $UNSIGNED_APP ]; then 
+    rm -v $UNSIGNED_APP
+fi
+
+# Zip all the html, javascript, CSS, images and other information.
+zip $UNSIGNED_APP *.html *.js *.css ./fonts/* ./img/* ./certificates/*.pem config.xml -x ./webinos.js
+
+
